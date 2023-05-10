e0acb603213240ef2468ead9ddb98777d6e321ac
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-05-10 20:06:27 +0500

    Redux toolkit basic setup

---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from './feature/cart/cartSlice';
+
+export const store = configureStore({
+    reducer: {
+        cart : cartReducer,
+    },
+})
+
+export default store;
